20f804e32285826f1560bb26f4d7f25b06189d73
bhagyatrivedi <144855580+bhagyatrivedi@users.noreply.github.com>
2024-01-08 18:03:23 -0500

    Update HomePage.js

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -35,13 +35,13 @@ const HomePage = () => {
       title: 'Eduversity - Decentralized Education System',
         description: 'I spearheaded the conception and execution of a groundbreaking decentralized digital educational platform, strategically shifting the focus from traditional institutions to empowering professors. With a vision to enhance global education accessibility and alleviate tuition costs, I championed the development of a fully decentralized system, granting professors autonomy over courses, schedules, and specialization. The implementation of a reputation index ensured transparent visibility into professors performance, aiding students in informed course selection. This inclusive platform facilitated remote professorial operations worldwide, breaking down geographical barriers through compatibility with tablets, browsers, and smart devices. From a streamlined startup process with a universal student directory to a robust transcript system enabling global course registration, the platform fostered efficiency and data analytics. Additionally, a graduation declaration process, a comprehensive course scheduling system, and an insightful dashboard for performance data collection were implemented, ensuring accountability and academic success. My innovative contributions, outlined in a detailed report and supported by an architecture diagram and class diagram, culminated in the successful development and deployment of a fully functional application. In conclusion, my work addresses the potential for transformative impact on education accessibility and affordability, particularly for the less fortunate, presenting a dynamic and evolving digital educational environment.',
         githubLink: 'https://github.com/bhagyatrivedi/java-project-eduversity',
-        image: `${aed2}`
+        image: `${aed2}`,
     }
     {
         title: 'OneBoston-Drug Regulator (Java & Java Swing based Full Stack Project)',
         description: 'The proposed approach involves developing a centralized platform that integrates bio-chemical compound data, disease information, and drug-related details. This platform should facilitate seamless communication between doctors, employees, and organizations, incorporating a role-based access system to ensure data security. Drug regulators can benefit from a dedicated module for monitoring and evaluating the safety and efficacy of drug compounds. Additionally, the system should include features for managing medical inventory within healthcare enterprises. A robust network infrastructure will underpin this solution, fostering collaboration, compliance, and information exchange, thereby advancing drug development and optimizing healthcare delivery in response to the evolving global health landscape.',
         githubLink: '',
-        image: `${aed}`
+        image: `${aed}`,
     },
     {
         title: 'E-Commerce Database Management System',
